54ef2fddc334ca10c962f8bc044f35688187b1c5
NCC74656 <wolginmark@gmail.com>
2017-02-10 10:36:59 -0500

    Update Robot_Control.ino
    Rewriting the code to meet the requirements for electric car

--- Robot_Control/Robot_Control.ino
+++ Robot_Control/Robot_Control.ino
@@ -2,102 +2,56 @@
 
 #include <Wire.h>
 
-byte bytes[16];
+byte bytes[3];
 
 //Servo wires
 //Brown - Ground
 //Red - Power
 //Orange - Signal
 
-Servo arm;
-Servo wrist;
-Servo hand;
-Servo cam;
+
 Servo cam2;
-Servo rise;
-Servo rise2;
 Servo forL;
 Servo forR;
 
-int risePin = 6; //triggers
-int rise2Pin = 7;
 int forLPin = 4; //left stick combo
 int forRPin = 5; //left stick combo
-int armPin = 10; //a and b
-int wristPin = 11;
-int handPin = 12; //bumpers
-int camPin = 8; //right stick x
 int cam2Pin = 9; //right stick y
 
-int armAngle = 90;
-int wristAngle = 90;
-int handAngle = 90;
-int camAngle = 90;
 int cam2Angle = 90;
 
 void setup() {
   Serial.begin(9600);
 
-  arm.attach(armPin);
-  arm.write(armAngle);
-    
-  wrist.attach(wristPin);
-  wrist.write(wristAngle);
-  
-  hand.attach(handPin);
-  hand.write(handAngle);
-
-  cam.attach(camPin);
-  cam.write(camAngle);
-
-  rise.attach(risePin);
-  rise.write(90);
-  rise2.attach(rise2Pin);
-  rise2.write(94);//this one is weird, it needs to be a little over 90
   forL.attach(forLPin);
   forR.attach(forRPin);
 
   cam2.attach(cam2Pin);
   cam2.write(cam2Angle);
   
-  delay(2000);
+  delay(2000);//in milliseconds
 }
 
 void loop() {
-  if(Serial.available() >= 16) {
+  if(Serial.available() >= 3) {
      while(Serial.read()!=250){} //only start taking data if it begins with this flag byte
-     for(int i = 0; i< 16; i++) {
+     for(int i = 0; i< 3; i++) {
         bytes[i] = Serial.read();
         if(bytes[i]==250) { //throw the array away if there's a 250 in it (as 250 is invalid)
           i=-1;
-          for(int j = 0; j< 16; j++)bytes[j]=0;
+          for(int j = 0; j< 3; j++)bytes[j]=0;
         }
-        //0: Left Stick Y, 0-100
-        //1: Left Stick X, 0-100
-        //2: Right Stick Y, 0-100
-        //3: Right Stick X, 0-100
-        //4: Triggers, 0-100
-        //5-8: A, B, X, Y
-        //9-10: Bumpers
-        //11-12: Back, Start
-        //13: Left Stick Button
-        //14: Right Stick Button
-        //15: Hat Switch, 0-8
-        //For Hat, 0 is resting, 1:NW, 2:N, continues clockwise
+        //0: Left Motor Y, 0-100
+        //1: Right Motor X, 0-100
+        //2: Turn Y, 0-180
      }
      //for(int i=0; i<16; i++) { //send data back to java so we know what we're doing
        //Serial.print(bytes[i]);
        //Serial.print("\t");
      //}
      //Serial.println();
-     if(bytes[0] <=60 && bytes[0] >=40)bytes[0]=50; //make sure we have a little tolerance
-     if(bytes[1] <=60 && bytes[1] >=40)bytes[1]=50;
-     if(bytes[2] <=60 && bytes[2] >=40)bytes[2]=50;
-     if(bytes[3] <=60 && bytes[3] >=40)bytes[3]=50;
-     if(bytes[4] <=53 && bytes[4] >=47)bytes[4]=50; //the triggers need less of a dead spot
      
      differentialDrive(bytes[1], bytes[0]);
-     iDontKnowWhyThisWorksButItDo(bytes[4]);
      
      cam2Angle = updateAngle(bytes[2], cam2Angle, 2);
      cam2Angle = constrain(cam2Angle, 60, 90);
@@ -105,23 +59,6 @@ void loop() {
      //Serial.print("Camera Angles: ");
      //Serial.print(cam2Angle);
      //Serial.print(" ");
-     
-     camAngle = updateAngle(bytes[3], camAngle, 2);
-     camAngle = constrain(camAngle, 60, 150);
-     cam.write(camAngle);
-     //Serial.println(camAngle);
-     
-     handAngle = servoIncrement(bytes[9], bytes[10], handAngle);
-     hand.write(handAngle);
-     
-     armAngle = servoIncrement(bytes[5], bytes[6], armAngle);
-     arm.write(armAngle);
-
-     wristAngle = servoIncrement(bytes[7], bytes[8], wristAngle); //this is for testing the servo ONLY
-     wrist.write(wristAngle);
-     
-     if(bytes[12]==1)pinMode(13,HIGH);
-     else pinMode(13,LOW); //debugging light, uses start button
   }
   delay(5); //just to make sure we don't update TOO fast
 }
@@ -161,13 +98,4 @@ void differentialDrive(int turn, int thrust) {
 //  Serial.print(map(rMotor,0,100,75,105)); 
 //  Serial.print(" L: ");
 //  Serial.println(map(lMotor,0,100,75,105));
-}
-
-void iDontKnowWhyThisWorksButItDo(byte angle) {
-  angle = map(angle, 0, 100, 60, 120);
-  Serial.print("Rise Angle: ");
-  Serial.println(angle);
-  rise.write(angle);
-  if(angle >=90 && angle <=94) rise2.write(94);
-  else rise2.write(angle);
 }
